41e8703df5c8e438281dc28eda72ff3d401fe02a
natalagoncaruk <honcharukna@gmail.com>
2025-03-03 23:49:21 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/viso-project/',
+  base: '/viso-project',
   plugins: [react()],
 });
